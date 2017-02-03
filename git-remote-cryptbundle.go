d6b2c6e17182e2d4a2e52b06557e614aa3bd9506
Vishvajit Singh <vish@vishsingh.com>
2017-02-02 18:28:16 -0800

    read lines from standard input

--- git-remote-cryptbundle.go
+++ git-remote-cryptbundle.go
@@ -3,9 +3,35 @@ package main
 import (
 	"log"
 	"fmt"
+	"bufio"
+	"os"
+	"io"
 )
 
+func doIt() error {
+	r := bufio.NewReader(os.Stdin)
+
+	for {
+		line, err := r.ReadString('\n')
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		line = line[0:len(line)-1]
+
+		fmt.Printf("I have read a line: '%s'\n", line)
+	}
+
+	return nil
+}
+
 func main() {
-	err := fmt.Errorf("unimplemented")
-	log.Fatal(err)
+	err := doIt()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
