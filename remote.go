a1eadb9c77b7014b2f6cc043c0155c6687fa5ac6
Vishvajit Singh <vish@vishsingh.com>
2017-02-06 21:49:01 -0800

    add fsRemote

--- remote.go
+++ remote.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"io"
+	"strings"
+	"os"
 	"fmt"
 )
 
@@ -13,6 +15,40 @@ type Remote interface {
 	PushBundle(encryptedBundleStream io.Reader) (newBundleName string, err error)
 }
 
+type fsRemote struct {
+	path string
+}
+
+func (r *fsRemote) Lock() error {
+	if r.path == "" {
+		return fmt.Errorf("fsRemote has blank path")
+	}
+
+	return os.Mkdir(r.path + "/lock", 0)
+}
+
+func (r *fsRemote) Unlock() error {
+	if r.path == "" {
+		return fmt.Errorf("fsRemote has blank path")
+	}
+
+	return os.Remove(r.path + "/lock")
+}
+
+func (r *fsRemote) GetBundles() ([]string, error) {
+	return nil, fmt.Errorf("fsRemote: GetBundles unimplemented")
+}
+
+func (r *fsRemote) PushBundle(ebs io.Reader) (string, error) {
+	return "", fmt.Errorf("fsRemote: PushBundle unimplemented")
+}
+
 func parseRemoteUrl(remoteUrl string) (Remote, error) {
+	if strings.HasPrefix(remoteUrl, "/") {
+		return &fsRemote {
+			path: remoteUrl,
+		}, nil
+	}
+
 	return nil, fmt.Errorf("unable to parse remote url %q", remoteUrl)
 }
