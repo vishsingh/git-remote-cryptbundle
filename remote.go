a0f5e300a7a938c0d11064e08be0886d716fd09a
Vishvajit Singh <vish@vishsingh.com>
2017-02-25 22:54:19 -0800

    implement PushBundle for fsRemote

--- remote.go
+++ remote.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"fmt"
 	"regexp"
+	"strconv"
 )
 
 type Remote interface {
@@ -37,6 +38,32 @@ func (r *fsRemote) Unlock() error {
 }
 
 var bundleNameRegex = regexp.MustCompile(`^[0-9]{8}\.bundle\.gpg$`)
+const bundleNameIndexLength = 8 // must match the regex
+const bundleNameFormatString = "%08d.bundle.gpg" // must match the regex
+
+func newBundleFilename(existingBundles []string) string {
+	newIndex := 0
+	for _, b := range existingBundles {
+		if len(b) < bundleNameIndexLength {
+			continue
+		}
+
+		numericPart := b[0:bundleNameIndexLength]
+
+		n, err := strconv.Atoi(numericPart)
+		if err != nil {
+			continue
+		}
+
+		if n < newIndex {
+			continue
+		}
+
+		newIndex = n + 1
+	}
+
+	return fmt.Sprintf(bundleNameFormatString, newIndex)
+}
 
 func (r *fsRemote) GetBundles() ([]string, error) {
 	fd, err := os.Open(r.path)
@@ -60,7 +87,44 @@ func (r *fsRemote) GetBundles() ([]string, error) {
 }
 
 func (r *fsRemote) PushBundle(ebs io.Reader) (string, error) {
-	return "", fmt.Errorf("fsRemote: PushBundle unimplemented")
+	// determine new filename based on existing bundles
+	existingBundles, err := r.GetBundles()
+	if err != nil {
+		return "", err
+	}
+
+	desiredFilename := newBundleFilename(existingBundles)
+
+	// exclusively create a file under r.path
+	desiredFilepath := r.path + "/" + desiredFilename
+	temporaryFilepath := desiredFilepath + ".new"
+
+	const openflags = os.O_WRONLY | os.O_APPEND | os.O_CREATE | os.O_EXCL
+	f, err := os.OpenFile(temporaryFilepath, openflags, 0600)
+	if err != nil {
+		return "", err
+	}
+
+	defer f.Close()
+	if _, err := io.Copy(f, ebs); err != nil {
+		return "", err
+	}
+
+	// atomically rename to desired filename
+	if err := os.Rename(temporaryFilepath, desiredFilepath); err != nil {
+		return "", err
+	}
+
+	// clear write bit
+	if err := f.Chmod(0400); err != nil {
+		return "", err
+	}
+
+	if err := f.Close(); err != nil {
+		return "", err
+	}
+
+	return desiredFilename, nil
 }
 
 func parseRemoteUrl(remoteUrl string) (Remote, error) {
