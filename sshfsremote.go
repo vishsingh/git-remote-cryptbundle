9835ccd250518cd2e3da16552f6287bde0ef346e
Vishvajit Singh <vish@vishsingh.com>
2017-03-01 23:00:56 -0800

    mount and unmount sshfs

--- sshfsremote.go
+++ sshfsremote.go
@@ -3,10 +3,11 @@ package main
 import (
 	"io"
 	//"strings"
-	//"os"
+	"os"
 	"fmt"
 	//"regexp"
 	//"strconv"
+	"os/exec"
 )
 
 type sshfsRemote struct {
@@ -14,6 +15,12 @@ type sshfsRemote struct {
 	// Will be the first argument passed to the 'sshfs' command.
 	// Looks like: user@host:dir
 	url string
+
+	// Temporary dir that serves as SSHFS mountpoint.
+	mountPoint string
+
+	// True while SSHFS is mounted.
+	mounted bool
 }
 
 func (*sshfsRemote) errNotImpl(method string) error {
@@ -21,11 +28,67 @@ func (*sshfsRemote) errNotImpl(method string) error {
 }
 
 func (r *sshfsRemote) Init() error {
-	return r.errNotImpl("Init")
+	// mkdir <mountpoint>
+	// sshfs <r.url> <mountpoint> <mount-options>
+
+	if r.url == "" {
+		return fmt.Errorf("sshfsRemote given empty URL")
+	}
+
+	home := os.Getenv("HOME")
+	if home == "" {
+		return fmt.Errorf("can not find user's HOME dir")
+	}
+
+	// todo: allow user to customize
+	mountPoint := home + "/.sshfs-mount"
+
+	if err := os.Mkdir(mountPoint, 0700); err != nil {
+		return fmt.Errorf("failed to create sshfs mountpoint: %v", err)
+	}
+
+	r.mountPoint = mountPoint
+
+	mountCmd := exec.Command("sshfs",
+		r.url,
+		mountPoint) // todo: options
+
+	out, err := mountCmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to mount sshfs: %q", string(out))
+	}
+
+	r.mounted = true
+
+	return nil
 }
 
 func (r *sshfsRemote) Uninit() error {
-	return r.errNotImpl("Uninit")
+	// fusermount -u <mountpoint>
+	// rmdir <mountpoint>
+
+	if r.mounted {
+		unmountCmd := exec.Command("fusermount",
+			"-u",
+			r.mountPoint)
+
+		out, err := unmountCmd.CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("failed to unmount sshfs: %q", string(out))
+		}
+
+		r.mounted = false
+	}
+
+	if r.mountPoint != "" {
+		if err := os.Remove(r.mountPoint); err != nil {
+			return fmt.Errorf("failed to remove sshfs mountpoint: %v", err)
+		}
+
+		r.mountPoint = ""
+	}
+
+	return nil
 }
 
 func (r *sshfsRemote) Lock() error {
